0b3267cd23428da6343b471dfc7f34a35edab027
WAHYU FATUR RIZKI <wahyufaturrizkyy@gmail.com>
2022-02-28 14:35:15 +0700

    feat: mobile res

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,10 @@
 import "styles/Main.css";
 import { ReactComponent as ImageGrow } from "assets/images/illustration-grow-together.svg";
+import { ReactComponent as ImageGrowMobile } from "assets/images/illustration-flowing-conversation-mobile.svg";
 import { ReactComponent as ImageFlow } from "assets/images/illustration-flowing-conversation.svg";
+import { ReactComponent as ImageFlowMobile } from "assets/images/illustration-grow-together-mobile.svg";
 import { ReactComponent as ImageUsers } from "assets/images/illustration-your-users.svg";
+import { ReactComponent as ImageUsersMobile } from "assets/images/illustration-your-users-mobile.svg";
 import Button from "./Button";
 
 export interface ListFeatInterface {
@@ -15,29 +18,57 @@ interface MainPropsInterface {
 const Main = (props: MainPropsInterface) => {
   return (
     <section className="container-main">
-      {props.listFeat.map((data, index) => (
-        <div
-          style={{ flexDirection: index === 1 ? "row-reverse" : undefined }}
-          key={index}
-          className="card-main"
-        >
-          <div className="text-image">
-            <h2 className="font-open-sans-700 very-dark-cyan">{data.title}</h2>
-            <h2 className="font-open-sans-400 grayish-blue mt-16">
-              {data.desc}
-            </h2>
+      <div className="hide-in-mobile">
+        {props.listFeat.map((data, index) => (
+          <div
+            style={{ flexDirection: index === 1 ? "row-reverse" : undefined }}
+            key={index}
+            className="card-main"
+          >
+            <div className="text-image">
+              <h2 className="font-open-sans-700 very-dark-cyan">
+                {data.title}
+              </h2>
+              <h2 className="font-open-sans-400 grayish-blue mt-16">
+                {data.desc}
+              </h2>
+            </div>
+            <div className="container-image">
+              {index === 0 ? (
+                <ImageGrow />
+              ) : index === 1 ? (
+                <ImageFlow />
+              ) : (
+                <ImageUsersMobile />
+              )}
+            </div>
           </div>
-          <div className="container-image">
-            {index === 0 ? (
-              <ImageGrow />
-            ) : index === 1 ? (
-              <ImageFlow />
-            ) : (
-              <ImageUsers />
-            )}
+        ))}
+      </div>
+
+      <div className="show-in-mobile">
+        {props.listFeat.map((data, index) => (
+          <div key={index} className="card-main">
+            <div className="text-image">
+              <h2 className="font-open-sans-700 very-dark-cyan">
+                {data.title}
+              </h2>
+              <h2 className="font-open-sans-400 grayish-blue mt-16">
+                {data.desc}
+              </h2>
+            </div>
+            <div className="container-image">
+              {index === 0 ? (
+                <ImageGrowMobile />
+              ) : index === 1 ? (
+                <ImageFlowMobile />
+              ) : (
+                <ImageUsers />
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
 
       <div className="container-ready">
         <h2 className="font-open-sans-700 very-dark-cyan">
